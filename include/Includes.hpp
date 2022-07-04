2a9695f0fc50c21c70dfa5bed353c16210d18fe7
PierreMauger <pierre.mauger@epitech.eu>
2022-07-04 20:45:40 +0200

    ADDED: Init project

---
+++ include/Includes.hpp
@@ -0,0 +1,15 @@
+#ifndef INCLUDES_HPP
+#define INCLUDES_HPP
+
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#endif // INCLUDES_HPP
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "Includes.hpp"
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
+{
+    return 0;
+}
