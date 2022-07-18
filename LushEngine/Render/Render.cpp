9f4ddbfb770e3b07c28908a171fd1eafd5583ef7
PierreMauger <pierre.mauger@epitech.eu>
2022-07-18 11:53:20 +0200

    ADDED: clear window

--- LushEngine/Render/Render.cpp
+++ LushEngine/Render/Render.cpp
@@ -17,7 +17,6 @@ Render::Render(std::shared_ptr<MessageBus> messageBus) : Node(messageBus)
         this->_messageBus->sendMessage(Message(Packet(), BaseCommand::QUIT, Module::BROADCAST));
     }
     glfwMakeContextCurrent(this->_window);
-
 }
 
 Render::~Render()
@@ -30,8 +29,13 @@ void Render::run()
     while (this->_running) {
         std::this_thread::sleep_for(std::chrono::milliseconds(16));
 
-        if (glfwWindowShouldClose(this->_window))
+        if (glfwWindowShouldClose(this->_window)) {
+            this->_running = false; // because it can sometimes send the quit message twice
             this->_messageBus->sendMessage(Message(Packet(), BaseCommand::QUIT, Module::BROADCAST));
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
         glfwPollEvents();
         glfwSwapBuffers(this->_window);
